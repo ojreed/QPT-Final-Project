cce7ac981d124ba697014d53c890e13fd1dc6231
Owen <oreed2@jhu.edu>
2024-03-05 18:26:53 -0500

    added more tests

--- SPYxLQD_histogram_test.py
+++ SPYxLQD_histogram_test.py
@@ -3,10 +3,34 @@ import Portfolio_Class as PF
 test = PF.Portfolio(["SPY","LQD"],[60,40],1000)
 print(test.get_value())
 print(test.get_asset_alloc())
-test.set_start(1,2,2009)
+test.set_start(1,1,2008)
 test.set_end_relative(5)
-print(test.start)
-print(test.end)
+while not test.is_done():
+	if test.is_first_of("month"):
+		test.rebalance()
+	test.update_next()
+print(test.get_value())
+print(test.get_asset_alloc())
+test.histogram(60)
+
+test = PF.Portfolio(["SPY","LQD"],[50,50],1000)
+print(test.get_value())
+print(test.get_asset_alloc())
+test.set_start(1,1,2008)
+test.set_end_relative(5)
+while not test.is_done():
+	if test.is_first_of("month"):
+		test.rebalance()
+	test.update_next()
+print(test.get_value())
+print(test.get_asset_alloc())
+test.histogram(60)
+
+test = PF.Portfolio(["SPY","LQD"],[80,20],1000)
+print(test.get_value())
+print(test.get_asset_alloc())
+test.set_start(1,1,2008)
+test.set_end_relative(5)
 while not test.is_done():
 	if test.is_first_of("month"):
 		test.rebalance()
@@ -18,10 +42,21 @@ test.histogram(60)
 test = PF.Portfolio(["SPY"],[100],1000)
 print(test.get_value())
 print(test.get_asset_alloc())
-test.set_start(1,2,2009)
+test.set_start(1,1,2008)
 test.set_end_relative(5)
-print(test.start)
-print(test.end)
+while not test.is_done():
+	if test.is_first_of("month"):
+		test.rebalance()
+	test.update_next()
+print(test.get_value())
+print(test.get_asset_alloc())
+test.histogram(60)
+
+test = PF.Portfolio(["LQD"],[100],1000)
+print(test.get_value())
+print(test.get_asset_alloc())
+test.set_start(1,1,2008)
+test.set_end_relative(5)
 while not test.is_done():
 	if test.is_first_of("month"):
 		test.rebalance()
